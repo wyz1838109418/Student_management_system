5c4815cff537dd397e0a5b1c8b25e615e3710502
wangyz <1838109418@qq.com>
2024-05-22 12:50:14 +0800

    Changes

--- src/course_about/SscController.java
+++ src/course_about/SscController.java
@@ -32,6 +32,9 @@ public class SscController {
     private TableView<select_course> fx_6;
 
     @FXML
+    private Button fx_9;
+
+    @FXML
     private void initialize() {
         // Initialize your TableView and set the items
         fx_3.setCellValueFactory(new PropertyValueFactory<>("sno"));
@@ -59,6 +62,30 @@ public class SscController {
             String cno = (String) rs.getString("cno");
             String score = (String) rs.getString("score");
             select_course s1 = new select_course(sno, cno, score);
+            data.add(s1);
+        }
+        fx_6.setItems(data);
+    }
+
+    @FXML
+    private void searchavg() throws ClassNotFoundException, SQLException {
+        ObservableList<select_course> data = FXCollections.observableArrayList();
+        String search_sno = fx_1.getText();
+        // 1. 注册驱动
+        Class.forName("com.mysql.cj.jdbc.Driver");// 建议写上，不写也不会报错
+        // 2. 获取数据库连接
+        String url = "jdbc:mysql://127.0.0.1:3306/?user=root";
+        String username = "root";
+        String password = "123456";
+        Connection conn = DriverManager.getConnection(url, username, password);
+        // 3. 定义sql
+        String sql = "SELECT sno,Student_management.avgscore(sno) as score FROM Student_management.select_course WHERE sno = '" + search_sno + "'";
+        Statement stmt = conn.createStatement();
+        ResultSet rs = stmt.executeQuery(sql);
+        if (rs.next()) {
+            String sno = (String) rs.getString("sno");
+            String score = (String) rs.getString("score");
+            select_course s1 = new select_course(sno, "", score);
             data.add(s1);
         }
         fx_6.setItems(data);
